a366a031a76000f4299972d7f56e1e6c0f14e6af
Dakota Mercer <dakotamercerwork@gmail.com>
2026-07-08 16:55:58 -0700

    added git ignore

--- build/control_framework_interfaces/ament_cmake_python/control_framework_interfaces/control_framework_interfaces/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from control_framework_interfaces.msg._control_input import ControlInput  # noqa: F401
-from control_framework_interfaces.msg._joint_state import JointState  # noqa: F401
--- build/excel_record_logging/CMakeFiles/excel_logger.dir/src/excel_logger.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/excel_logger.dir/src/excel_logger.cpp.o: \
- /home/dakota/.local/repos/control_framework/src/excel_record_logging/src/excel_logger.cpp \
- /usr/include/stdc-predef.h
