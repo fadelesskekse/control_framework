05a4cd2f49ea1dd764e65a1ffd5424f81a80cfac
Dakota Mercer <dakotamercerwork@gmail.com>
2026-07-11 17:07:18 -0700

    Stopped tracking build install log stuff

--- build/control_framework_interfaces/control_framework_interfaces__rs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dakota/.local/repos/control_framework/build/control_framework_interfaces/control_framework_interfaces__rs
-# Build directory: /home/dakota/.local/repos/control_framework/build/control_framework_interfaces/control_framework_interfaces__rs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/sim/CMakeFiles/parallel_sim.dir/src/parallel_sim.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/parallel_sim.dir/src/parallel_sim.cpp.o: \
- /home/dakota/.local/repos/control_framework/src/lockstep_sim/src/parallel_sim.cpp \
- /usr/include/stdc-predef.h
